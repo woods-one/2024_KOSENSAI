c55f6b9d7ad2b27d8b41ca227e902d8049380850
woods-one <0820ricer@gmail.com>
2024-09-11 03:27:57 +0900

    first commit

---
+++ Assets/Test.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    void Start()
+    {
+        var hoge = true;
+        if (hoge)
+        {
+            Debug.Log("テストだよ");
+        }
+    }
+}
